3534d066a691d8abcf8d7cdc112119ef5b8c51bf
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-09-11 15:33:02 +0300

    Added comments

--- complete.R
+++ complete.R
@@ -1,4 +1,7 @@
 complete <- function(directory, id=1:332) {
+        ## COMPLETE returns a dataframe of 2 col: id (file id) and num_observations
+        # of complete cases in the corresponding file id.
+        ## Takes in directory name and file ids
         df <- as.data.frame(matrix(data=NA, nrow=length(id), ncol=2))
         names(df) <- c("id", "nobs")
         
